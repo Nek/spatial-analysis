4c1fce37653fe34ba3e42dfd80074e7e389e9fe7
Nikita Dudnik <nikdudnik@gmail.com>
2023-07-19 09:50:52 +0200

    Clean up

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    preact({
+      babel: {
+        plugins: ['@babel/plugin-transform-react-jsx-source'],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      react: 'preact/compat',
+      'react-dom': 'preact/compat',
+      'react-reconciler': 'preact-reconciler',
+    },
+  },
 })
