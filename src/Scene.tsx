0b54350cd10cb6e7623050a85ccc3018f8679986
Nikita Dudnik <nikdudnik@gmail.com>
2023-08-04 02:14:04 +0200

    Tweak controls

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -1,8 +1,10 @@
 import { CameraControls, TransformControls, Sphere, Select, Cone } from '@react-three/drei'
-import { Object3DNode, extend, useFrame } from '@react-three/fiber'
-import { RefObject, Suspense, useEffect, useRef, useState } from 'react'
+import { Suspense, useRef, useState } from 'react'
 import useHotkeys from '@reecelucas/react-use-hotkeys'
 import { Group, type Object3D } from 'three'
+
+import { Smush32 } from '@thi.ng/random'
+const rnd = new Smush32(0)
 
 // declare module '@react-three/fiber' {
 //   interface ThreeElements {
@@ -15,20 +17,6 @@ function Light() {
     <directionalLight position={[5, 5, -8]} castShadow intensity={1.5} shadow-mapSize={2048} shadow-bias={-0.001}>
       <orthographicCamera attach="shadow-camera" args={[-8.5, 8.5, 8.5, -8.5, 0.1, 20]} />
     </directionalLight>
-  )
-}
-
-const EditableCone = () => {
-  const mesh = useRef(null)
-  useEffect(() => console.log(mesh.current), [mesh.current])
-  return (
-    <>
-      <mesh ref={mesh}>
-        <coneGeometry args={[1, 3, 32]} />
-        <meshBasicMaterial color={'red'} opacity={0.5} />
-      </mesh>
-      <TransformControls object={mesh.current!} mode="translate" />
-    </>
   )
 }
 
@@ -55,21 +43,13 @@ function Scene() {
     setSpace(space === 'world' ? 'local' : 'world')
   })
 
-  const [orbit, setOrbit] = useState(false)
-
+  const [orbit, setOrbit] = useState(true)
   useHotkeys(' ', (event) => {
     setOrbit(!orbit)
     event.preventDefault()
   })
 
   const objectsG = useRef<Group | null>(null)
-
-  // useFrame(() => {
-  //   if (objectsG.current) {
-  //     console.log(objectsG.current?.children[0])
-  //     // objectsG.current?.children.forEach((o) => console.log(o))
-  //   }
-  // })
 
   return (
     <Suspense fallback={<Sphere />}>
@@ -79,22 +59,25 @@ function Scene() {
       <Light />
       {active && (
         <TransformControls
+          showZ={transformMode === 'rotate'}
+          showX={transformMode !== 'rotate'}
+          showY={transformMode !== 'rotate'}
           mode={transformMode}
           object={active}
           space={space}
           onObjectChange={(e) => console.log(e?.target.object)}
         />
       )}
-      {/*<mesh>*/}
-      {/*  <coneGeometry args={[1, 3, 32]} />*/}
-      {/*  <meshBasicMaterial color={'red'} opacity={0.5} />*/}
-      {/*</mesh>*/}
-      <Select box multiple={true} onChange={(e) => setSelected(e)}>
+      <Select box multiple={true} onChange={(e) => setSelected(e)} onClick={(e) => setSelected([e.object])}>
         <group ref={objectsG}>
           {[...Array(5)].map((_, i) => {
             return (
-              <group key={'cone' + i} position={[0, 6, 0]}>
-                <Cone args={[1, 12, 24]} position={[0, 0, 0]}>
+              <group
+                key={'cone' + i}
+                rotation={[0, 0, (180 * Math.PI) / 180, 'XYZ']}
+                position={[rnd.minmax(-3, 3), rnd.minmax(1, 1.5), 0]}
+              >
+                <Cone args={[1, 12, 24]} position={[0, -6, 0]}>
                   <meshBasicMaterial color={'rgb(164,84,217)'} opacity={0.25} transparent={true} depthWrite={false} />
                 </Cone>
               </group>
